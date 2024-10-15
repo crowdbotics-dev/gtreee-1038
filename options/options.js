3a55d084507ba341704e616f632e88a44064c139
Crowdbotics <engineering@crowdbotics.com>
2024-10-15 05:22:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gtreee-1038",
+  url: "https://gtreee-1038.botics.co",
+  api: "https://gtreee-1038.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
